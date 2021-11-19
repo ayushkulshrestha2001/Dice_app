93712683e18e23fa84185bc1986b2fb742548bf1
Ayush Kulshrestha <ayushkulshrestha2001@gmail.com>
2021-11-19 12:02:22 +0530

    completed dice project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,7 +8,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Dice'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +17,52 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    void change() {
+      setState(() {
+        leftDice = (Random().nextInt(6)) + 1;
+        rightDice = (Random().nextInt(6)) + 1;
+      });
+    }
+
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1, //for telling how much should this one expand
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  change();
+                });
+              },
+              child: Image(
+                image: AssetImage('images/dice$leftDice.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: TextButton(
+              onPressed: () {
+                change();
+              },
+              child: Image(
+                image: AssetImage('images/dice$rightDice.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
